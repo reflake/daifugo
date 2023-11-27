b1dde828f2c80ff1b9d5272b47ad43d164eceb95
reflake <reflakeykt@gmail.com>
2023-11-27 22:05:59 +0600

    describe a function returning card values

--- Program.fs
+++ Program.fs
@@ -3,12 +3,32 @@ type Rank = | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jac
 type Card = | RegularCard of Suit * Rank
             | Joker
 
+let cardValue card =
+    
+    match card with
+    | RegularCard (_, rank) ->
+        match rank with
+        | Two -> 2
+        | Three -> 3
+        | Four -> 4
+        | Five -> 5
+        | Six -> 6
+        | Seven -> 7
+        | Eight -> 8
+        | Nine -> 9
+        | Ten -> 10
+        | Jack -> 11
+        | Queen -> 12
+        | King -> 13
+        | Ace -> 14
+    | Joker -> 15
+
 [<EntryPoint>]
 let main _ =
     
     let myCard   = RegularCard(Hearts, Two)
     let yourCard = Joker
     
-    printfn "My card is %O" myCard
-    printfn "Your card is %O" yourCard
+    printfn "My card is value %O" (myCard |> cardValue)
+    printfn "Your card is value %O" (yourCard |> cardValue)
     0 // return an integer exit code
