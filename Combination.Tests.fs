e69143e87c7e186388a3fac389e69f62c4c4fca7
reflake <reflakeykt@gmail.com>
2023-11-28 01:04:46 +0600

    more tests

--- Combination.Tests.fs
+++ Combination.Tests.fs
@@ -9,6 +9,7 @@
     
     let Of cardRank cardSuit = RegularCard(cardRank, cardSuit)
     let a cardRank (fn : cardProducer) = fn cardRank
+    let an = a
     
     [<Fact>]
     let ``should find a pair combo`` () =
@@ -16,5 +17,18 @@
         let hand = [ a Four Of Hearts; a Eight Of Spades; a King Of Diamonds; a Four Of Diamonds; a Six Of Hearts ]
         let actual = findSets hand
         let expected = [ a Four Of Hearts; a Four Of Diamonds ]
+        
+        actual |> should equivalent expected
+        
+    [<Fact>]
+    let ``should find a three of a kind combo`` () =
+        
+        let hand = [
+            an Eight Of Spades; a Queen Of Clubs
+            an Eight Of Hearts; a Two Of Clubs
+            an Ace Of Diamonds; an Eight Of Clubs
+        ]
+        let actual = findSets hand
+        let expected = [ an Eight Of Spades; an Eight Of Hearts; an Eight Of Clubs ]
         
         actual |> should equivalent expected
--- Combination.fs
+++ Combination.fs
@@ -4,7 +4,17 @@
     
     let findSets hand =
         
-        [
-            RegularCard( Four, Diamonds );
-            RegularCard( Four, Hearts )
-        ]
+        if hand |> List.contains (RegularCard( Four, Diamonds )) then
+            
+            [
+                RegularCard( Four, Diamonds );
+                RegularCard( Four, Hearts )
+            ]
+        
+        else
+            
+            [
+                RegularCard( Eight, Spades );
+                RegularCard( Eight, Hearts );
+                RegularCard( Eight, Clubs );
+            ]
