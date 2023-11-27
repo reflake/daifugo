93927abcaaa21c879cb44064e998b9bc37b83c80
reflake <reflakeykt@gmail.com>
2023-11-27 21:44:00 +0600

    describe Card type

--- Program.fs
+++ Program.fs
@@ -1,4 +1,14 @@
+type Suit = | Hearts | Diamonds | Clubs | Spades
+type Rank = | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
+type Card = | RegularCard of Suit * Rank
+            | Joker
+
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
+let main _ =
+    
+    let myCard   = RegularCard(Hearts, Two)
+    let yourCard = Joker
+    
+    printfn "My card is %O" myCard
+    printfn "Your card is %O" yourCard
     0 // return an integer exit code
