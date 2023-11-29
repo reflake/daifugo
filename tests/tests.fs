11b59be5d5cdbc019fbeb6ae571f3ca6fef67dfe
reflake <reflakeykt@gmail.com>
2023-11-29 16:52:47 +0600

    move tests to separate library

---
+++ tests/tests.fs
@@ -0,0 +1,11 @@
+﻿module tests.tests
+
+    open NUnit.Framework
+    open FsUnit
+    
+    let add a b = a + b
+    
+    [<Test>]
+    let ``hello world`` () =
+        
+        add 2 2 |> should equal 4
