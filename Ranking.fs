8c4300503836d751087f389c162451f80a326664
reflake <reflakeykt@gmail.com>
2023-12-02 11:24:59 +0600

    refactoring

--- Ranking.fs
+++ Ranking.fs
@@ -10,16 +10,18 @@
         
         let (|First|Second|Middle|PreLast|Last|) =
                
-               let threePlayers = numberOfPlayers = 3
-               let preLastPlace = numberOfPlayers - 2
-               let lastPlace = numberOfPlayers - 1
-               
-               function
-               | Some(0) -> First
-               | Some(1) when not threePlayers -> Second
-               | Some(place) when not threePlayers && place = preLastPlace -> PreLast
-               | Some(place) when place = lastPlace -> Last
-               | _ -> Middle
+            let threePlayers = numberOfPlayers = 3
+            let preLastPlace = numberOfPlayers - 2
+            let lastPlace = numberOfPlayers - 1
+            
+            function
+            | Some place ->
+                match place with
+                | 0 -> First
+                | 1 when not threePlayers -> Second
+                | place when not threePlayers && place = preLastPlace -> PreLast
+                | place when place = lastPlace -> Last
+                | _ -> Middle
         
         match place with
         | First -> Tycoon
